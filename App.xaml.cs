bc23355444ba3d01b73657b9e158cc32a62d049f
ilyesstourki <tourkiilyess@gmail.com>
2025-01-05 15:37:37 +0100

    Beta version p1
    part 1

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using TrainSheet.View;
+
+namespace TrainSheet;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new MuscleType());
+	}
+}
